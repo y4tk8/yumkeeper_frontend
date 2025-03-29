637b9edfe061c1cd4aeb9e26bcdb2854d0d426f5
Yoshitaka Ide <yoshi4363@gmail.com>
2025-03-29 15:38:47 +0900

    feat: コンテンツ幅を制御するContainerコンポーネントを定義 #9

---
+++ src/components/layout/Container.tsx
@@ -0,0 +1,17 @@
+import React from "react";
+
+interface ContainerProps {
+  children: React.ReactNode;
+}
+
+const Container: React.FC<ContainerProps> = ({ children }) => {
+  return (
+    <div className="w-full min-h-screen bg-gray-100">
+      <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
+        {children}
+      </div>
+    </div>
+  )
+}
+
+export default Container;
