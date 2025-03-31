ae2f29f9505fb0eeb641a3be167eaedf20adf671
Yoshitaka Ide <yoshi4363@gmail.com>
2025-03-31 23:54:46 +0900

    chore: 開発時に各タグの領域を識別しやすいよう一時的にCSSを変更

--- src/components/layout/Container.tsx
+++ src/components/layout/Container.tsx
@@ -6,7 +6,7 @@ interface ContainerProps {
 
 const Container: React.FC<ContainerProps> = ({ children }) => {
   return (
-    <div className="w-full min-h-screen bg-gray-100">
+    <div className="w-full min-h-screen bg-gray-600">
       <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
         {children}
       </div>
