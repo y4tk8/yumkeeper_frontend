c94497670e35d503ded32a6516a02c7702637e1c
Yoshitaka Ide <yoshi4363@gmail.com>
2025-04-14 15:00:21 +0900

    fix: name, amountの両方が空の行はmap前に除外するよう修正 #58

--- src/utils/mapItems.ts
+++ src/utils/mapItems.ts
@@ -3,7 +3,9 @@ import { ItemEntry } from "@/types/recipe";
 
 // 材料 or 調味料 の入力値を map で処理
 export const mapItems = (items: ItemEntry[], category: "ingredient" | "seasoning") => {
-  return items.map((item) => {
+  return items
+  .filter((item) => item.name.trim() !== "" || item.amount.trim() !== "") // name と amount どちらも空の行は除外
+  .map((item) => {
     const { quantity, unit } = parseAmountToQuantityAndUnit(item.amount);
     return {
       name: item.name,
