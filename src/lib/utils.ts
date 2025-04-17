71652041986424bafe97d70fe2e365a2b54b725d
Yoshitaka Ide <yoshi4363@gmail.com>
2025-04-17 16:56:01 +0900

    chore: shadcn/ui をインストール #64

---
+++ src/lib/utils.ts
@@ -0,0 +1,8 @@
+// shadcnコンポーネントに関する設定
+
+import { clsx, type ClassValue } from "clsx"
+import { twMerge } from "tailwind-merge"
+
+export function cn(...inputs: ClassValue[]) {
+  return twMerge(clsx(inputs))
+}
