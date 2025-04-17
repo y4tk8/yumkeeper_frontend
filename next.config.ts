386abf1e86d0930b364b2537aa91589ce9b7820e
Yoshitaka Ide <yoshi4363@gmail.com>
2025-04-17 17:02:03 +0900

    chore: YouTubeのサムネイル画像表示のためのドメイン許可を追加 #64

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,9 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  images: {
+    domains: ["i.ytimg.com"], // YouTubeのサムネイル画像のドメインを許可
+  },
 };
 
 export default nextConfig;
