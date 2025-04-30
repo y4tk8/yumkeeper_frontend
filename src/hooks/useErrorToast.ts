4a84880bbbd81bcd353e7987baa763df92d9ccd9
Yoshitaka Ide <yoshi4363@gmail.com>
2025-04-30 16:45:46 +0900

    fix: 認証済みならセッションストレージからauthErrorを削除するよう修正 #96

--- src/hooks/useErrorToast.ts
+++ src/hooks/useErrorToast.ts
@@ -1,22 +1,40 @@
 "use client";
 
-import { useEffect } from "react";
+import { useEffect, useContext } from "react";
+import { AuthContext } from "@/contexts/AuthContext";
 import { showErrorToast } from "@/components/ui/shadcn/sonner";
 
 // ステータスコード分岐でエラートーストを表示するフック
 export const useErrorToast = () => {
+  const context = useContext(AuthContext);
+
+  // AuthProvider でのラップ漏れがあった際にエラーを出す
+  if (!context) {
+    throw new Error("useErrorToast must be used within an AuthProvider");
+  }
+
+  const { isAuthenticated } = context;
+
   useEffect(() => {
     if (typeof window === "undefined") return; // SSR対策
 
     const authError = sessionStorage.getItem("authError");
     const forbiddenError = sessionStorage.getItem("forbiddenError");
 
-    if (authError) {
+    if (!isAuthenticated && authError) {
       showErrorToast(authError);
       sessionStorage.removeItem("authError");
-    } else if (forbiddenError) {
+    }
+
+    // 認証済み & authError有り ならセッションストレージから削除
+    // NOTE: 初回マウントでAPIリクエストがある場合、`認証判定前` にauthErrorがセットされるため
+    if (isAuthenticated && authError) {
+      sessionStorage.removeItem("authError");
+    }
+
+    if (forbiddenError) {
       showErrorToast(forbiddenError);
       sessionStorage.removeItem("forbiddenError");
     }
-  }, []);
-}
+  }, [isAuthenticated]);
+};
