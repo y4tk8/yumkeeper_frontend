ff4fb66b07affd0fb835ab4168b5936c8600a3af
Yoshitaka Ide <yoshi4363@gmail.com>
2025-05-05 00:58:24 +0900

    chore: トップページにコピー文とサインアップボタンを設置 #125

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,24 +1,36 @@
 "use client";
 
-export default function Home() {
-  // APIレスポンス取得のハンドラー
-  const handleApiRequest = async () => {
-    const response = await fetch("http://localhost:8080");
-    const text = response.text();
-    console.log(text);
-  };
+import Link from "next/link";
 
+export default function Home() {
   return (
-    <div className="grid min-h-screen grid-rows-[20px_1fr_20px] items-center justify-items-center gap-16 p-8 pb-20">
-      <main className="row-start-2 flex flex-col items-center gap-8">
-        {/* APIリクエストの送信ボタン */}
-        <button
-          onClick={handleApiRequest}
-          className="rounded-full border border-black px-6"
-        >
-          APIリクエスト
-        </button>
-      </main>
-    </div>
+    <section className="py-32">
+      {/* メインコピー */}
+      <h1 className="text-7xl font-bold text-left">
+        料理レシピは活字より、<br />
+        <span className="inline-block w-full text-right mt-8">動画派のあなたへ。</span>
+      </h1>
+
+      {/* サブコピー */}
+      <div className="mt-20 text-center">
+        <p className="text-2xl">
+          <span className="text-3xl font-semibold">Yum Keeper</span>は、動画に特化した料理レシピ保存サービスです。
+        </p>
+        <p className="mt-4 text-2xl">
+          YouTubeの動画と共にレシピをシンプルに管理しよう！
+        </p>
+      </div>
+
+      {/* ボタン */}
+      <div className="mt-32 text-center">
+        <Link href="/signup">
+          <button className="px-8 py-4 rounded-full bg-black text-white text-lg font-semibold hover:bg-gray-700 transition">
+            登録する
+          </button>
+        </Link>
+        <p className="text-lg mt-4">ゲストログイン機能を用意しています</p>
+        <p className="text-lg mt-1">（メールアドレス・パスワードの登録不要）</p>
+      </div>
+    </section>
   );
 }
