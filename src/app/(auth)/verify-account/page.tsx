913009e852635e6c050008aebc2e4365c7416633
Yoshitaka Ide <yoshi4363@gmail.com>
2025-05-04 13:11:27 +0900

    chore: 認証メール確認依頼ページのUIを改善

--- src/app/(auth)/verify-account/page.tsx
+++ src/app/(auth)/verify-account/page.tsx
@@ -4,20 +4,23 @@ import Link from "next/link";
 
 const VerifyAccountPage = () => {
   return (
-    <div className="flex flex-col items-center justify-center min-h-screen px-6">
-      <div className="w-full max-w-lg bg-white p-6 rounded-md border border-gray-300 shadow-sm text-center">
-        <h2 className="text-xl font-semibold mb-6">本登録はまだ完了していません</h2>
+    <>
+      <div className="text-center space-y-10">
+        <h2 className="text-xl">登録ありがとうございます</h2>
+        <h2 className="text-2xl font-semibold mb-6">本登録を完了するにはメールを認証してください</h2>
         <ul className="text-gray-600 text-sm list-disc list-inside mb-6 text-left">
           <li>通常、メールは数分以内に届きます。</li>
           <li>メールが見つからない場合、迷惑メールフォルダに振り分けられていることがあります。</li>
           <li>しばらく経ってもメールが届かない場合はメールアドレスが正しいか確認の上、再送をお試しください。</li>
         </ul>
+      </div>
 
+      <div className="text-center mt-10">
         <Link href="/resend-confirmation" className="text-blue-600 hover:underline">
           認証メールを再送する場合はこちら
         </Link>
       </div>
-    </div>
+    </>
   );
 };
 
