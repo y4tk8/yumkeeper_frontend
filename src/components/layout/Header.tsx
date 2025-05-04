bf52a5b3d624dafb94c86eebd6325b74b7ebf833
Yoshitaka Ide <yoshi4363@gmail.com>
2025-05-05 00:59:52 +0900

    chore: ヘッダーのパディング変更とサインアップボタンを設置 #125

--- src/components/layout/Header.tsx
+++ src/components/layout/Header.tsx
@@ -7,21 +7,33 @@ import Link from "next/link";
 export default function Header() {
   const context = useContext(AuthContext);
 
+  // AuthProvider でのラップ漏れがあった際にエラーを出す
   if (!context) {
-    throw new Error("useApiClient must be used within an AuthProvider.");
+    throw new Error("Header must be used within an AuthProvider.");
   }
 
   const { authHeaders } = context;
 
   return (
-    <header className="fixed top-0 left-0 w-full z-50 bg-gray-100 shadow-md flex items-center justify-between p-4">
+    <header className="fixed top-0 left-0 w-full z-50 bg-gray-100 shadow-md flex items-center justify-between px-24 py-4">
       <Link href="/">
-        <h1 className="text-xl font-bold">Yum Keeper</h1>
+        <h1 className="text-2xl font-bold">Yum Keeper</h1>
       </Link>
       <nav>
-        <ul className="flex gap-4">
-          {authHeaders ? "サインイン中" : ""}
-          <li><Link href="/signin">サインイン</Link></li>
+        <ul className="flex items-center gap-4">
+          {authHeaders && <li>ログイン中</li>}
+
+          <li>
+            <Link href="/signup">
+              <button className="px-4 py-2 rounded-full bg-black text-white hover:bg-gray-700 transition">
+                登録する
+              </button>
+            </Link>
+          </li>
+
+          <li>
+            <Link href="/signin" className="hover:underline">ログイン</Link>
+          </li>
         </ul>
       </nav>
     </header>
