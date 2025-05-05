69c223551c786167b59fb2137dcba4b4aeb62d6d
Yoshitaka Ide <yoshi4363@gmail.com>
2025-05-05 18:56:21 +0900

    chore: 認証後トップページUIとuseContextによる認証判定ロジックを追加 #127

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,8 +1,52 @@
 "use client";
 
+import { useContext } from "react";
+import { AuthContext } from "@/contexts/AuthContext";
 import Link from "next/link";
+import { Plus } from "lucide-react";
 
 export default function Home() {
+  const context = useContext(AuthContext);
+
+  // AuthProvider のラップ漏れチェック
+  if (!context) {
+    throw new Error("Home must be used within an AuthProvider.");
+  }
+
+  const { isAuthenticated, isAuthChecked } = context;
+
+  if (!isAuthChecked) return; // 認証チェック中は何も描画しない（チラつき防止）
+
+  // 認証後のトップページ
+  if (isAuthenticated) {
+    return (
+      <section className="py-32">
+        {/* コピー文 */}
+        <h2 className="text-4xl font-semibold text-left">
+          レシピ探しも、管理も、これひとつでOK。
+        </h2>
+        <h2 className="text-4xl font-semibold text-left mt-6">
+          保存しておけば、作りたくなる日が来る。
+        </h2>
+
+        <div className="mt-56 flex items-center gap-16">
+          <Link href="/recipes/new">
+            <button className="flex items-center gap-2 px-8 py-4 rounded-full bg-black text-white text-lg font-semibold hover:bg-gray-700 transition ml-2">
+              <Plus size={20}/> レシピを追加する
+            </button>
+          </Link>
+
+          <Link href="/recipes/index">
+            <span className="text-xl text-gray-700 hover:underline">
+              レシピ一覧へ
+            </span>
+          </Link>
+        </div>
+      </section>
+    );
+  }
+
+  // 認証前のトップページ
   return (
     <section className="py-32">
       {/* メインコピー */}
@@ -21,7 +65,7 @@ export default function Home() {
         </p>
       </div>
 
-      {/* ボタン */}
+      {/* サインアップボタン */}
       <div className="mt-32 text-center">
         <Link href="/signup">
           <button className="px-8 py-4 rounded-full bg-black text-white text-lg font-semibold hover:bg-gray-700 transition">
