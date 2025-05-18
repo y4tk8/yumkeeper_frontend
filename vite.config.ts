1a866076f5d4781bbc4dc6c86bf1793c3735094b
Yoshitaka Ide <yoshi4363@gmail.com>
2025-05-19 00:55:27 +0900

    test: vitest用の初期設定ファイルやプラグインを読み込み #175

---
+++ vite.config.ts
@@ -0,0 +1,17 @@
+/// <reference types="vitest" />
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
+import path from "path";
+
+export default defineConfig({
+  plugins: [react()],
+  test: {
+    environment: "jsdom",
+    setupFiles: "./vitest-setup.ts",
+  },
+  resolve: {
+    alias: {
+      "@": path.resolve(__dirname, "./src"),
+    },
+  },
+});
