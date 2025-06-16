4a353904f87d78604e1ae12ade5f03372b0a4ca7
Yoshitaka Ide <yoshi4363@gmail.com>
2025-06-16 15:51:23 +0900

    fix: 共通cssではなく個別にTailwindを設定 #199

--- src/app/not-found.tsx
+++ src/app/not-found.tsx
@@ -1,6 +1,6 @@
 import { Metadata } from "next";
 import Image from "next/image";
-import "@/app/styles/error.css"
+import Container from "@/components/layout/Container";
 
 export const metadata: Metadata = {
   title: "404 Not Found",
@@ -8,34 +8,39 @@ export const metadata: Metadata = {
 
 export default function NotFound() {
   return (
-    <main role="main">
-      <div className="content" aria-labelledby="error-heading">
-        <Image
-          src="/images/404.png"
-          alt="ページが見つかりませんでした"
-          width={9999}
-          height={9999}
-          style={{
-            maxHeight: "340px",
-            height: "auto",
-            width: "auto",
-          }}
-          priority
-        />
-        <div className="error-text">
-          <div className="error-code">404</div>
-          <div className="error-message" id="error-heading">
-            Not Found
+    <Container>
+      <main className="flex flex-col items-center justify-center min-h-screen text-center font-sans">
+        <div className="flex items-center">
+          <Image
+            src="/images/404.png"
+            alt="ページが見つかりませんでした"
+            width={9999}
+            height={9999}
+            style={{
+              maxHeight: "340px",
+              height: "auto",
+              width: "auto",
+            }}
+            priority
+          />
+          <div className="flex flex-col justify-center h-full">
+            <div className="text-[6rem] font-bold leading-[2]">404</div>
+            <div className="text-[2.5rem] font-normal" id="error-heading">
+              Not Found
+            </div>
           </div>
         </div>
-      </div>
 
-      <p className="description">
-        お探しのページは見つかりませんでした。
-      </p>
-      <a href="https://www.yumkeeper.net" className="back-link">
-        トップへ戻る
-      </a>
-    </main>
+        <p className="mt-12 text-[1.1rem] text-black">
+          お探しのページは見つかりませんでした。
+        </p>
+        <a
+          href="https://www.yumkeeper.net"
+          className="mt-10 text-[1.1rem] text-blue-600 hover:underline"
+        >
+          トップへ戻る
+        </a>
+      </main>
+    </Container>
   );
 }
